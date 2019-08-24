5d583f96ffa75e875b2b2b2a92be4a9abb5a6230
Johnny <johnnybcoding@gmail.com>
2019-08-24 09:53:25 -0700

    first commit

---
+++ src/ValidatedLoginForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Formik } from "formik";
+import * as EmailValidator from "email-validator";
+import * as Yup from "yup";
+
+const ValidatedLoginForm = () => {
+  <div>
+    <h1>Hello World</h1>
+  </div>;
+};
+
+export default ValidatedLoginForm;
